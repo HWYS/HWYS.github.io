e2ba9cd2cbce2255e0c41b05495771233b144ef8
HtetWaiYanSwe <HtetWaiYanSwe@users.noreply.github.com>
2021-01-20 20:35:27 +0630

    Add two modals <100 and >=100

--- game.js
+++ game.js
@@ -437,34 +437,42 @@ class playGame extends Phaser.Scene {
                 fontStyle: 'bold',
             }
             //Show Messagebox
-            if (this.score < 100) {
+            if (this.score < 10) {
                 //this.showMessageBox("Hit a 100 score to get a chance to spin the wheel", this.width * .7, this.height * .5)
-                var rect = this.add.rectangle(380, 120, 355, 200, 0xd3d3d3);
+                /*var rect = this.add.rectangle(380, 120, 355, 200, 0xd3d3d3);
                 let msg = this.add.text(245, 70, "Hit a 100 score\n to get a chance to spin the wheel", textStyle);
 
                 this.playAgainBtn = this.add.text(340, 170, "Play Again", textStyle)
                     .on('pointerover', () => this.enterButtonHoverState())
-                    .on('pointerdown', () => this.playAgain());
+                    .on('pointerdown', () => this.playAgain());*/
 
-                /*playAgainBtn = this.add.rectangle(270, 150, 100, 30, 0x00136f)
-                .setInteractive()
-                .on('pointerover', () => this.enterButtonHoverState() );*/
+                    this.cacheScene = this.scene;
+                    this.scene.pause();
+                    $('#mdl').modal('toggle');
+                    var vm = this;
+                    $('#btnTryAgain').click(function (e) {
+                        vm.scene.start();
+                    })
+            }
+            else {
+                this.cacheScene = this.scene;
+                this.scene.pause();
+                $('#goToSpinnWheel').modal('toggle');
+                    var vm = this;
+                    $('#btnTryAgain').click(function (e) {
+                        vm.scene.start();
+                    })
 
-                //playAgainBtn.onInputOver.add(over, this);
-                //playAgainBtn.onInputOut.add(out, this);
-                //playAgainBtn.onInputUp.add(up, this);
-                //let playAgainBtn = this.add.button(300, 180, "Play Again");
+                    $('#btnGoToSpinnWheel').click(function (e) {
+                        vm.scene.start();
+                        window.location.assign("https://spin-wheel.cbtp.ovh/?playTimes=10&spinnCount=10&bestScore=100&msid=3133352556771057");
+                    })
+                    //this.scene.start("PlayGame");   
             }
 
             // restart the game
             //this.scene.start("PlayGame");
-            this.cacheScene = this.scene;
-            this.scene.pause();
-            $('#mdl').modal('toggle');
-            var vm = this;
-            $('#btnTryAgain').click(function (e) {
-                vm.scene.start();
-            })
+            
         }
 
         // adjust 3D ball position
@@ -472,83 +480,5 @@ class playGame extends Phaser.Scene {
         this.ball3D.position.x = this.ball.x * gameOptions.gameScale;
 
     }
-
-    playAgain() {
-        alert('You Loose');
-    }
-
-    enterButtonHoverState() {
-        this.playAgainBtn.setStyle({fill: '#ffffff'});
-    }
-
-    showMessageBox(text, w = 300, h = 300) {
-        //just in case the message box already exists
-        //destroy it
-        if (this.msgBox) {
-            this.msgBox.destroy();
-        }
-        //make a group to hold all the elements
-        var msgBox = game.add.group();
-        //make the back of the message box
-        var back = this.add.sprite(0, 0, "boxBack");
-        //make the close button
-        var closeButton = this.add.sprite(0, 0, "closeButton");
-        //make a text field
-        var text1 = this.add.text(0, 0, text);
-        //set the textfeild to wrap if the text is too long
-        text1.wordWrap = true;
-        //make the width of the wrap 90% of the width 
-        //of the message box
-        text1.wordWrapWidth = w * .9;
-        //
-        //
-        //set the width and height passed
-        //in the parameters
-        back.width = w;
-        back.height = h;
-        //
-        //
-        //
-        //add the elements to the group
-        msgBox.add(back);
-        msgBox.add(closeButton);
-        msgBox.add(text1);
-        //
-        //set the close button
-        //in the center horizontally
-        //and near the bottom of the box vertically
-        closeButton.x = back.width / 2 - closeButton.width / 2;
-        closeButton.y = back.height - closeButton.height;
-        //enable the button for input
-        closeButton.inputEnabled = true;
-        //add a listener to destroy the box when the button is pressed
-        closeButton.events.onInputDown.add(this.hideBox, this);
-        //
-        //
-        //set the message box in the center of the screen
-        msgBox.x = this.width / 2 - msgBox.width / 2;
-        msgBox.y = this.height / 2 - msgBox.height / 2;
-        //
-        //set the text in the middle of the message box
-        text1.x = back.width / 2 - text1.width / 2;
-        text1.y = back.height / 2 - text1.height / 2;
-        //make a state reference to the messsage box
-        this.msgBox = msgBox;
-    }
-
-    up() {
-
-    }
-
-    over() {
-
-    }
-
-    out() {
-
-    }
-
-    actionOnClick() {
-
-    }
+    
 }
